3602e9649e7cd5262e753994888de462f2aa3adf
moonzawa <yuzuki.kitazawa@metateam.co.jp>
2025-04-24 16:46:00 +0900

    書籍登録

--- src/main/java/jp/co/metateam/library/controller/BookController.java
+++ src/main/java/jp/co/metateam/library/controller/BookController.java
@@ -88,8 +88,6 @@ public class BookController {
             log.error("書籍情報の保存に失敗しました",e);
             model.addAttribute("errorMessage","書籍情報の保存中にエラーが発生しました。もう一度お試しください。");
             
-            this.bookMstService.save(bookMstDto);
-            
             return "book/add";
         }
     }
